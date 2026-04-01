2996d84a87667a75df673ea9e389793405f5e3de
akashsingh2127 <akash.vidyagyan11@gmail.com>
2026-04-01 17:33:59 +0530

    Implement Euclidean algorithm to compute GCD and add executable file

---
+++ basic_math/euclidean_algo_GCD.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n1,n2;
+    cin>>n1>>n2;
+    int maximum=max(n1,n2);
+    int minimum=min(n1,n2);
+    int remainder=maximum%minimum;
+    while(minimum!=0){  
+        remainder=maximum % minimum;     
+        maximum = minimum;
+        minimum=remainder;        
+    }
+    cout<<maximum;
+
+    return 0;
+}
+
+
+
+
+/*
+/*
+Euclidean Algorithm finds GCD using: GCD(a, b) = GCD(b, a % b).
+We repeatedly replace (maximum, minimum) with (minimum, maximum % minimum).
+The loop continues until minimum becomes 0.
+The final value of maximum is the GCD.
+
+Time Complexity:
+O(log(min(a, b))) 
+
+Space Complexity: 
+O(1)
+
+*/
