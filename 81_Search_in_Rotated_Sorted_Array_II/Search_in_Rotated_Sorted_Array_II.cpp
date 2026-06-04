869ff4e3ced33959d46336ad9a2cd0780cbb57fb
akashsingh2127 <akash.vidyagyan11@gmail.com>
2026-06-04 20:11:51 +0530

    Refactor search function in Search in Rotated Sorted Array II

--- 81_Search_in_Rotated_Sorted_Array_II/Search_in_Rotated_Sorted_Array_II.cpp
+++ 81_Search_in_Rotated_Sorted_Array_II/Search_in_Rotated_Sorted_Array_II.cpp
@@ -22,54 +22,40 @@ public:
             if(nums[mid] == target) {
                 return true;
             }
-
             //this is the additional line in Problem No. 33
             if(nums[start] == nums[mid] &&
                nums[mid] == nums[end]) {
-
                 start++;
                 end--;
             }
 
             else if(nums[start] <= nums[mid]) {
-
                 if(target >= nums[start] &&
                    target < nums[mid]) {
-
                     end = mid - 1;
                 }
-
                 else {
-
                     start = mid + 1;
                 }
             }
-
             else {
-
                 if(target > nums[mid] &&
                    target <= nums[end]) {
-
                     start = mid + 1;
                 }
-
                 else {
-
                     end = mid - 1;
                 }
             }
         }
-
         return false;
     }
 };
 
 int main() {
     Solution s;
-
     vector<int> nums = {2, 5, 6, 0, 0, 1, 2};
     int target = 0;
-
     if(s.search(nums, target))
         cout << "Target Found";
     else
